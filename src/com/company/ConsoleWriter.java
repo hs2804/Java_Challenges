f7ba940e82ea13822a2e6683fd8a1eb8c6dee261
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2020-08-17 11:03:45 -0400

    Setup JAVA

---
+++ src/com/company/ConsoleWriter.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class ConsoleWriter {
+
+    public void printOnConsole(String str){
+        System.out.println(str);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ConsoleWriter console = new ConsoleWriter();
+        console.printOnConsole("Hello, World.");
+        console.printOnConsole("Hello, Java.");
+
+    }
+}
